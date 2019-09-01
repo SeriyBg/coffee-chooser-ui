d0b848f82037615756f5f88b63f10c27078d56b6
Serge Bishyr <seriybg@gmail.com>
2019-09-01 14:54:12 +0300

    setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware');
 
-// const service_url = process.env.SERVICE_URL || 'http://localhost:8080/';
-//
-// module.exports = function(app) {
-//     app.use('/coffee', proxy({ target: service_url}));
-// };
+const service_url = process.env.SERVICE_URL || 'http://localhost:8080/';
+
+module.exports = function(app) {
+    app.use('/coffee', proxy({ target: service_url}));
+};
